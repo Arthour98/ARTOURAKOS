8301bd59f7351a82ec719124b600dd5b8cde4e81
Arthour98 <artourbouloudis@gmail.com>
2025-02-21 01:33:09 +0200

    Initial commiwth HTML,CSS,JS,and JSON files

---
+++ script.js
@@ -0,0 +1,36 @@
+$("ul li:eq(0)").on("click",function()
+{
+window.location="index.html";
+})
+$("ul li:eq(1)").on("click",function()
+{
+window.location="drag-drop.html";
+})
+$("ul li:eq(2)").on("click",function()
+{
+window.location="testings.html";
+})
+
+$("button#show-display").on("click",function()
+{
+$("p#hidden-text").slideToggle();
+})
+
+let click_more_button=$("button#show-display");
+click_more_button.on("mouseover",function()
+{
+$(this).css(
+    {
+    "background-color":"green"
+    }
+)
+}).delay(2000).on("mouseleave",function()
+{
+$(this).css(
+    {
+    "background-color":"coral"
+    }
+)
+})
+
+
